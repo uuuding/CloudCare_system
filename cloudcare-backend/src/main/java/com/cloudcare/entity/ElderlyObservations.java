279be88637353a92f4dbb76bbb0872566a36c811
menheraneko <1718973225@qq.com>
2025-06-28 23:02:27 +0800

    风险

--- cloudcare-backend/src/main/java/com/cloudcare/entity/ElderlyObservations.java
+++ cloudcare-backend/src/main/java/com/cloudcare/entity/ElderlyObservations.java
@@ -1,5 +1,6 @@
 package com.cloudcare.entity;
 
+import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
 import com.baomidou.mybatisplus.extension.activerecord.Model;
@@ -13,15 +14,35 @@ public class ElderlyObservations extends Model<ElderlyObservations> {
 
     @TableId
     private Integer id;
+    
+    @TableField("elderly_id")
     private Integer elderlyId;
+    
+    @TableField("observation_time")
     private String observationTime;
+    
+    @TableField("body_temperature")
     private Double bodyTemperature;
+    
+    @TableField("systolic_bp")
     private Integer systolicBp;
+    
+    @TableField("cough")
     private Boolean cough;
+    
+    @TableField("heart_rate")
     private Integer heartRate;
-    private Double sleep_hours;
+    
+    @TableField("sleep_hours")
+    private Double sleepHours;
+    
+    @TableField("observation_location")
     private String observationLocation; // 新增属性，记录观察地点
+    
+    @TableField("height")
     private Double height; // 身高(cm)
+    
+    @TableField("weight")
     private Double weight; // 体重(kg)
 
 }
